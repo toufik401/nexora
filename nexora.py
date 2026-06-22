a69577995538d34ba60b9d6e8810e520b0e4ae25
toufik401 <toufiiktito40@gmail.com>
2026-06-22 19:19:56 +0100

    Update nexora.py

--- nexora.py
+++ nexora.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import requests
+
+# 1. إعدادات الصفحة
 st.set_page_config(page_title="معاً لنطور تجارة مدينتنا", layout="centered")
+
 # 2. CSS للخلفية والعنوان الذهبي
-st.markdown(""
+st.markdown("""
     <style>
     .stApp {
         background-image: url("https://img.freepik.com/free-vector/dark-background-with-golden-lines_1017-27083.jpg");
@@ -34,7 +37,12 @@ st.write("<br>", unsafe_allow_html=True)
 
 with st.container():
     st.markdown("<div class='content-box'>", unsafe_allow_html=True)
-    st.markdown("""<a href="https://ibb.co/TMc4W7GS"><img src="https://i.ibb.co/v413XTWG/1782140096443.png" alt="1782140096443" border="0"></a><br /><a target='_blank' href='https://usefulwebtool.com/fr/convertir-texte-en-binaire'>que veut dire binaire</a><br />")
+    # تم تصحيح الروابط والتنسيق هنا
+    st.markdown("""
+        <a href="https://ibb.co/TMc4W7GS"><img src="https://i.ibb.co/v413XTWG/1782140096443.png" alt="1782140096443" border="0"></a><br />
+        <a target='_blank' href='https://usefulwebtool.com/fr/convertir-texte-en-binaire'>ما معنى كلمة ثنائي (binaire)؟</a><br />
+    """, unsafe_allow_html=True)
+    
     st.image(st.session_state.img, use_container_width=True)
     st.markdown(f"<h2 style='text-align: center; color: black;'>السعر: {st.session_state.price} دج</h2>", unsafe_allow_html=True)
     st.markdown("</div>", unsafe_allow_html=True)
@@ -55,9 +63,14 @@ with st.container():
                 token = "8640762406:AAF540rnfipL54HSUIRZqODSsBcQjM2uybo"
                 chat_id = "7055252264"
                 msg = f"🛒 طلب جديد!\nالاسم: {name}\nالهاتف: {phone}\nالنشاط: {trade_type}\nالسعر: {st.session_state.price}"
-                requests.post(f"https://api.telegram.org/bot{token}/sendMessage", data={"chat_id": chat_id, "text": msg})
-                st.balloons()
-                st.success("تم إرسال طلبك بنجاح!")
+                
+                response = requests.post(f"https://api.telegram.org/bot{token}/sendMessage", data={"chat_id": chat_id, "text": msg})
+                
+                if response.status_code == 200:
+                    st.balloons()
+                    st.success("تم إرسال طلبك بنجاح!")
+                else:
+                    st.error("حدث خطأ أثناء الإرسال")
             else:
                 st.error("يرجى ملء البيانات المطلوبة")
     st.markdown("</div>", unsafe_allow_html=True)
